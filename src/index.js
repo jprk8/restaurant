b8052a12f63d3bda9a10425412efd37051932d70
John Park <176120986+jprk8@users.noreply.github.com>
2024-08-23 16:09:27 -0700

    Make menu module and tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import './style.css';
 import { showHome } from './home.js';
+import { showMenu } from './menu.js';
 
 console.log('cock-a-doodle-doo');
-
 showHome();
+
+const homeBtn = document.querySelector('.home-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const aboutBtn = document.querySelector('.about-btn');
+
+function refreshContent() {
+    const body = document.querySelector('body');
+    const oldContent = document.getElementById('content');
+    body.removeChild(oldContent);
+    const newContent = document.createElement('div');
+    newContent.id = 'content';
+    const footer = document.querySelector('.footer');
+    body.insertBefore(newContent, footer);
+}
+
+homeBtn.addEventListener('click', () => {
+    refreshContent();
+    showHome();
+});
+
+menuBtn.addEventListener('click', () => {
+    refreshContent();
+    showMenu();
+});
---
+++ src/menu.js
@@ -0,0 +1,85 @@
+export { showMenu };
+import hotwing from './img/hotwing.png';
+
+class Food {
+    constructor(name, description, price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+const rooster = new Food('Rooster Boy', 'our most popular! (50g protein)', 49);
+const bonnie = new Food('Bonnie and Cluck', 'for two (100g protein)', 89);
+const hungry = new Food('Hungry Hungry Hicken', 'for the whole family (200g protein)', 179);
+const chick = new Food('Chick Possible', 'for our young patrons (20g protein)', 25);
+
+const foods = [];
+foods.push(rooster);
+foods.push(bonnie);
+foods.push(hungry);
+foods.push(chick);
+
+function showMenu() {
+    const content = document.getElementById('content');
+
+    const menuTitle = document.createElement('div');
+    menuTitle.classList.add('menu-title');
+    menuTitle.textContent = 'FOOD MENU';
+    content.appendChild(menuTitle);
+
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menu-container');
+
+    for (const food of foods) {
+        const item = document.createElement('div');
+        item.classList.add('menu-item');
+
+        const name = document.createElement('div');
+        const description = document.createElement('div');
+        const price = document.createElement('div');
+        name.classList.add('name');
+        description.classList.add('description');
+        price.classList.add('price');
+        name.textContent = food.name;
+        description.textContent = food.description;
+        price.textContent = food.price;
+
+        item.appendChild(name);
+        item.appendChild(description);
+        item.appendChild(price);
+
+        menuContainer.appendChild(item);
+    }
+
+    // Add rooster boy icon
+    const icon = document.createElement('img');
+    icon.classList.add('hoticon');
+    icon.src = hotwing;
+    menuContainer.appendChild(icon);
+
+    // Attach food menu to #content
+    content.appendChild(menuContainer);
+
+    const sauceTitle = document.createElement('div');
+    sauceTitle.classList.add('menu-title');
+    sauceTitle.textContent = 'SAUCES';
+    content.appendChild(sauceTitle);
+
+    const sauce1 = document.createElement('span');
+    sauce1.classList.add('sauce');
+    const highlight = document.createElement('span');
+    highlight.classList.add('highlight');
+    const sauce2 = document.createElement('span');
+    sauce2.classList.add('sauce');
+
+    sauce1.textContent = `Every food item includes Roosterbilt's`;
+    highlight.textContent = '5 Alltaste Flavor Sauces';
+    sauce2.textContent = `\nRandomly generated from a proprietary algorithm using all possible taste a human tastebud can taste.
+    Remember Every Flavour Beans? Yea it's like that.
+    Taste the rainbow.`;
+
+    sauce1.appendChild(highlight);
+    sauce1.appendChild(sauce2);
+    content.appendChild(sauce1);
+}
